4986bab950d1262d95bdfb4cc8de0b36287f00b6
jaycdev <93813551+jaycdev@users.noreply.github.com>
2024-04-13 12:03:39 +1000

    fix: add file input link to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,5 +7,6 @@
 </header>
 
 <ScrollArea class="flex flex-col">
-	<div><a href="/select-enum-example">Select component example using enums</a></div>
+	<div>&rArr; <a href="/select-enum-example" class="hover:text-orange-500">Select component example using enums</a></div>
+	<div>&rArr; <a href="/file-input-example" class="hover:text-orange-500">An example using file inputs</a></div>
 </ScrollArea>
